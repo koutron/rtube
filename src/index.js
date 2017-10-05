f37bf614a2d432a4d95fb14c822b5ddb2fafa88f
koutron <kamousavi@gmail.com>
2017-10-05 10:53:08 -0400

    heroku upload

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyDvMYGwwOQpT5X6NRXNYoT0dRiioHEwJxQ';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('Javascript');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term }, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+			// same as this.setState({ videos: videos});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce(term => {
+			this.videoSearch(term);
+		}, 300);
+		return (
+			<div className="wrapper">
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+					videos={this.state.videos}
+				/>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));

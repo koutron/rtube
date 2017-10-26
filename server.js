a359eb375f3529302b30989796da9df0b3a4ff07
koutron <kamousavi@gmail.com>
2017-10-26 12:20:21 -0400

    server.js fixed

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static('/build'));
-const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(5000);
